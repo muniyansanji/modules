9f8669b20a6cbecb2e8bccfc31787a0e5a57cfea
muni <muniyanabi11@gmail.com>
2023-07-05 12:44:27 +0530

    recode the project

---
+++ module.py
@@ -0,0 +1,29 @@
+import os
+
+#current file path show 
+'''
+print(os.getcwd())
+'''
+#currently file path working or not working
+'''
+print(help(os.getcwd))
+'''
+#file path change - os.chdir("path")
+'''
+os.chdir(r"C:\Users\muniy\OneDrive\Documents\django_prop")
+print(os.getcwd())
+'''
+#file path new folder create os.mkdir("") then folder to new folder
+'''
+os.mkdir(r"C:\Users\muniy\OneDrive\Documents\django_prop")
+print(os.getcwd())
+'''
+#file path folder rename create os.rename("old name","new name")
+''''
+os.rename("muniyan","ram",)
+print(os.getcwd())
+'''
+#file path remove 
+'''
+os.rmdir(r"C:\Users\muniy\OneDrive\Documents\django_prop\ram\ram\ram")
+'''
